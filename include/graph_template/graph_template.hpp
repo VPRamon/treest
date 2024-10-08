972a8c1231bfd865a9b6073bfe8101a3a416fd38
valles <valles@ice.csic.es>
2024-10-08 22:31:11 +0200

    optimize and document graph template

--- include/graph_template/graph_template.hpp
+++ include/graph_template/graph_template.hpp
@@ -8,49 +8,66 @@ namespace vpr {
 namespace templates {
 
 /**
- * @class Graph
- * @brief A base graph implementation using a vector to store nodes.
- *
- * @tparam Node The type of the nodes in the graph.
+ * @brief Generic container representing a graph structure.
+ * 
+ * This templated graph class manages nodes and provides functionalities to add, access, and iterate over them.
+ * It offers flexibility by allowing the use of custom containers and allocators.
+ * 
+ * @tparam Node The type representing a node in the graph.
+ * @tparam Container The container type used to store nodes. Defaults to std::vector.
+ * @tparam Allocator The allocator type for the nodes. Defaults to std::allocator.
  */
 template <typename Node, template <typename, typename> class Container = std::vector, typename Allocator = std::allocator<Node>>
 class Graph {
 protected:
-
+    /**
+     * @brief Container holding all nodes in the graph.
+     * 
+     * The underlying container type is user-defined, allowing flexibility for different performance requirements.
+     */
     Container<Node, Allocator> nodes_;
 
 public:
-
     /**
-     * @brief Constructs a Graph with an optional initial capacity for nodes.
+     * @brief Constructs an empty graph with an optional initial capacity.
      * 
-     * @param initialCapacity Initial capacity for the nodes vector. Defaults to 16.
+     * @param initialCapacity Initial reserve capacity for the container to minimize reallocations.
+     * @param alloc Allocator instance used to manage memory for the container.
      */
-    explicit Graph(size_t initialCapacity = 16) {
+    explicit Graph(size_t initialCapacity = 16, const Allocator& alloc = Allocator())
+        : nodes_(alloc) {
         nodes_.reserve(initialCapacity);
     }
 
     /**
-     * @brief Copy constructor. Performs a deep copy of the graph.
-     *
-     * @param other The Graph to copy from.
+     * @brief Copy constructor.
+     * 
+     * Creates a deep copy of the given graph, including allocating memory using the same allocator.
+     * 
+     * @param other The graph to copy from.
      */
-    Graph(const Graph& other) : nodes_(other.nodes_)
-    { }
+    Graph(const Graph& other) : nodes_(other.nodes_.get_allocator()) {
+        nodes_.reserve(other.nodes_.size());
+        nodes_ = other.nodes_;
+    }
 
     /**
-     * @brief Move constructor. Transfers ownership of the graph from another Graph.
-     *
-     * @param other The Graph to move from.
+     * @brief Move constructor.
+     * 
+     * Transfers ownership of the resources from the given graph to this instance, avoiding deep copies.
+     * 
+     * @param other The graph to move from.
      */
     Graph(Graph&& other) noexcept : nodes_(std::move(other.nodes_))
     { }
 
     /**
-     * @brief Copy assignment operator. Performs a deep copy of the graph.
-     *
-     * @param other The Graph to copy from.
-     * @return Reference to the assigned Graph.
+     * @brief Copy assignment operator.
+     * 
+     * Assigns the state of another graph to this graph, performing a deep copy of nodes.
+     * 
+     * @param other The graph to copy from.
+     * @return A reference to the assigned graph.
      */
     Graph& operator=(const Graph& other) {
         if (this != &other) {
@@ -60,132 +77,145 @@ public:
     }
 
     /**
-     * @brief Move assignment operator. Transfers ownership of the graph from another Graph.
-     *
-     * @param other The Graph to move from.
-     * @return Reference to the assigned Graph.
+     * @brief Move assignment operator.
+     * 
+     * Transfers ownership of the resources from the given graph to this instance.
+     * 
+     * @param other The graph to move from.
+     * @return A reference to the assigned graph.
      */
     Graph& operator=(Graph&& other) noexcept {
-        if (this != &other) {
-            nodes_ = std::move(other.nodes_);
-        }
+        nodes_ = std::move(other.nodes_);
         return *this;
     }
 
     /**
-     * @brief Constructs a node with arguments and adds it to the graph.
-     *
-     * @tparam Args Types of arguments to construct the node.
-     * @param args Arguments needed to construct the node.
-     * @return The index of the newly added node.
+     * @brief Clears all nodes from the graph.
+     * 
+     * This operation leaves the graph empty but maintains the current capacity of the underlying container.
      */
-    template <typename... Args>
-    size_t emplace_node(Args&&... args) {
-        size_t nodeIndex = nodes_.size();
-        nodes_.emplace_back(nodeIndex, std::forward<Args>(args)...);
-        return nodeIndex;
+    void clear() noexcept {
+        nodes_.clear();
     }
 
     /**
-     * @brief Retrieves a reference to the node at the specified index.
-     *
-     * @param index The index of the node.
-     * @return Reference to the node.
+     * @brief Constructs a new node in place.
      * 
-     * @throws std::out_of_range if the index is invalid.
+     * Adds a new node to the graph, forwarding arguments to the node constructor.
+     * 
+     * @tparam Args Variadic template for forwarding arguments to the node constructor.
+     * @param args Arguments used to construct the new node.
+     * @return The index of the newly created node.
+     */
+    template <typename... Args>
+    size_t emplace_node(Args&&... args) {
+        size_t node_index = nodes_.size();
+        nodes_.emplace_back(node_index, std::forward<Args>(args)...);
+        return node_index;
+    }
+
+    /**
+     * @brief Access a node by its index.
+     * 
+     * Throws an exception if the index is out of bounds.
+     * 
+     * @param index The index of the node to access.
+     * @return A reference to the node at the specified index.
+     * @throw std::out_of_range If the index is invalid.
      */
     Node& getNode(size_t index) { return nodes_.at(index); }
 
     /**
-     * @brief Retrieves a const reference to the node at the specified index.
-     *
-     * @param index The index of the node.
-     * @return Const reference to the node.
+     * @brief Access a node by its index (const version).
      * 
-     * @throws std::out_of_range if the index is invalid.
+     * Throws an exception if the index is out of bounds.
+     * 
+     * @param index The index of the node to access.
+     * @return A const reference to the node at the specified index.
+     * @throw std::out_of_range If the index is invalid.
      */
     const Node& getNode(size_t index) const { return nodes_.at(index); }
 
     /**
-     * @brief Retrieves the number of nodes_ in the graph.
-     *
-     * @return The number of nodes.
+     * @brief Returns the number of nodes in the graph.
+     * 
+     * @return The current number of nodes.
      */
-    constexpr size_t size() const noexcept { return nodes_.size(); }
+    inline size_t size() const noexcept { return nodes_.size(); }
 
     /**
      * @brief Checks if the graph is empty.
-     *
-     * @return true if the graph has no nodes_, false otherwise.
+     * 
+     * @return True if there are no nodes, false otherwise.
      */
-    constexpr bool empty() const noexcept { return nodes_.empty(); }
-
-    // *** Iterator Methods ***
+    inline bool empty() const noexcept { return nodes_.empty(); }
 
     /**
      * @brief Returns an iterator to the beginning of the nodes.
-     *
-     * @return Iterator to the beginning of the nodes.
+     * 
+     * @return Iterator pointing to the first node.
      */
-    auto begin() { return nodes_.begin(); }
+    typename Container<Node, Allocator>::iterator begin() noexcept { return nodes_.begin(); }
 
     /**
-     * @brief Returns an iterator to the end of the nodes_.
-     *
-     * @return Iterator to the end of the nodes.
+     * @brief Returns an iterator to the end of the nodes.
+     * 
+     * @return Iterator pointing past the last node.
      */
-    auto end() { return nodes_.end(); }
+    typename Container<Node, Allocator>::iterator end() noexcept { return nodes_.end(); }
 
     /**
-     * @brief Returns a reverse iterator to the beginning of the nodes.
-     *
-     * @return Reverse iterator to the beginning of the nodes.
+     * @brief Returns a reverse iterator to the beginning of the nodes in reverse order.
+     * 
+     * @return Reverse iterator pointing to the last node.
      */
-    auto rbegin() { return nodes_.rbegin(); }
+    typename Container<Node, Allocator>::reverse_iterator rbegin() noexcept { return nodes_.rbegin(); }
 
     /**
-     * @brief Returns a reverse iterator to the end of the nodes.
-     *
-     * @return Reverse iterator to the end of the nodes.
+     * @brief Returns a reverse iterator to the end of the nodes in reverse order.
+     * 
+     * @return Reverse iterator pointing before the first node.
      */
-    auto rend() { return nodes_.rend(); }
+    typename Container<Node, Allocator>::reverse_iterator rend() noexcept { return nodes_.rend(); }
 
     /**
-     * @brief Returns a constant iterator to the beginning of the nodes.
-     *
-     * @return Constant iterator to the beginning of the nodes.
+     * @brief Const version of the iterator to the beginning of nodes.
+     * 
+     * @return Const iterator pointing to the first node.
      */
-    const auto begin() const { return nodes_.begin(); }
+    typename Container<Node, Allocator>::const_iterator begin() const noexcept { return nodes_.begin(); }
 
     /**
-     * @brief Returns a constant iterator to the end of the nodes.
-     *
-     * @return Constant iterator to the end of the nodes.
+     * @brief Const version of the iterator to the end of nodes.
+     * 
+     * @return Const iterator pointing past the last node.
      */
-    const auto end() const { return nodes_.end(); }
+    typename Container<Node, Allocator>::const_iterator end() const noexcept { return nodes_.end(); }
 
     /**
-     * @brief Returns a constant reverse iterator to the beginning of the nodes.
-     *
-     * @return Constant reverse iterator to the beginning of the nodes.
+     * @brief Const version of the reverse iterator to the beginning of nodes in reverse order.
+     * 
+     * @return Const reverse iterator pointing to the last node.
      */
-    const auto rbegin() const { return nodes_.rbegin(); }
+    typename Container<Node, Allocator>::const_reverse_iterator rbegin() const noexcept { return nodes_.rbegin(); }
 
     /**
-     * @brief Returns a constant reverse iterator to the end of the nodes.
-     *
-     * @return Constant reverse iterator to the end of the nodes.
+     * @brief Const version of the reverse iterator to the end of nodes in reverse order.
+     * 
+     * @return Const reverse iterator pointing before the first node.
      */
-    const auto rend() const { return nodes_.rend(); }
+    typename Container<Node, Allocator>::const_reverse_iterator rend() const noexcept { return nodes_.rend(); }
 
     /**
-     * @brief Overloads the output stream operator to print all nodes in the graph.
-     *
-     * @param os Output stream.
-     * @param graph The graph to print.
-     * @return Reference to the output stream.
+     * @brief Outputs the graph to an output stream.
+     * 
+     * Iterates over all nodes and writes their values to the given output stream.
+     * 
+     * @param os The output stream.
+     * @param graph The graph to output.
+     * @return A reference to the output stream.
      */
-    friend std::ostream& operator<<(std::ostream& os, const Graph<Node, Container, Allocator>& graph) {
+    friend std::ostream& operator<<(std::ostream& os, const Graph& graph) {
         for (const auto& node : graph.nodes_) {
             os << node << " ";
         }
@@ -193,28 +223,27 @@ public:
     }
 
 protected:
-
     /**
-     * @brief Adds a directed edge from one node to another.
-     *
-     * @param from The index of the source node.
-     * @param to The index of the target node.
+     * @brief Adds an edge between two nodes.
      * 
-     * @throws std::out_of_range if either index is invalid.
+     * Validates indices before creating a connection from one node to another.
+     * 
+     * @param from Index of the starting node.
+     * @param to Index of the target node.
+     * @throw std::out_of_range If either index is invalid.
      */
     void addEdge(size_t from, size_t to) {
         validateIndex(to);
         nodes_.at(from).addEdge(to);
     }
 
-protected:
-
     /**
-     * @brief Validates if a node index is within bounds.
-     *
-     * @param index The index to validate.
+     * @brief Ensures the given index is valid for accessing nodes.
      * 
-     * @throws std::out_of_range if the index is invalid.
+     * Throws an exception if the index is out of range.
+     * 
+     * @param index Index to validate.
+     * @throw std::out_of_range If the index is invalid.
      */
     inline void validateIndex(size_t index) const {
         if (index >= nodes_.size()) {
